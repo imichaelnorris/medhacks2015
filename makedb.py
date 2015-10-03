90707401132a43991fce140949fc212f0e7a2fe0
Michael Norris <michael@Michaels-MacBook-Pro.local>
2015-10-03 11:23:30 -0400

    stuff

--- makedb.py
+++ makedb.py
@@ -5,11 +5,13 @@ import db
 f = open('patient_data.json')
 
 data = json.loads(f.read())
-for record in data['000']:
-    if record['type'] == 'img':
-        link = record['value']
-        img = io.imread(link)
+db.patients.remove()
+for patient in data['patients']:
+    for record in patient['records']:
+        if record['type'] == 'img':
+            link = record['value']
+            img = io.imread(link)
 
-        record['value'] = base64.b64encode(img)
+            record['value'] = base64.b64encode(img)
+    db.patients.insert(patient)
 
-db.patients.insert(data)
